4441b77277a0fa1b7d0a0148273830d4e31e281a
Alexey Illarionov <alexey@0xdc.ru>
2021-07-03 12:53:15 +0300

    Пример с отменой и ожиданиями корутин, запущенных в других скоупах

--- src/main/kotlin/coroutines/ParentalResponsibilities.kt
+++ src/main/kotlin/coroutines/ParentalResponsibilities.kt
@@ -1,11 +1,15 @@
 package coroutines
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-// parent-корутина ждет своих child'ов, join не обязателен
-fun main() = runBlocking {
+fun main() {
+    //`parent-корутина ждет своих child'ов`()
+    `withContext и отмены корутин, запущенных в других скоупах`()
+}
+
+//
+fun `parent-корутина ждет своих child'ов`() = runBlocking {
     val request = launch {
         repeat(3) { i -> // launch a few children jobs
             launch {
@@ -19,3 +23,49 @@ fun main() = runBlocking {
     println("Now processing of the request is complete")
 }
 
+fun `withContext и отмены корутин, запущенных в других скоупах`() {
+
+    // Репозиторий со своим скоупом.
+    class Repository: CoroutineScope {
+        val job: Job = Job()
+        override val coroutineContext: CoroutineContext
+            get() =  job + Dispatchers.IO
+
+        suspend fun getRandomNumberAfterDelay(): Float {
+            log("getRandomNumber()")
+            // Корутины запускаются с Job от Repository в качестве parent'а.
+            val result = withContext(coroutineContext) {
+                log("inside coroutineContext")
+                delay(3000)
+                log("delay complete, i'm still alive") // {1}
+                42f
+            }
+            log("getRandomNumber() result: $result")
+            return result
+        }
+    }
+
+    val repository = Repository()
+    runBlocking {
+        val globalJob = GlobalScope.launch() {
+            val newValue = repository.getRandomNumberAfterDelay()
+            log("GlobalJob still alive. NewValue: $newValue") // {2}
+        }
+        delay(300)
+
+        // Если отменить globalJob, то он не отменит delay в getRandomNumberAfterDelay(), так как там withContext с другим parent'ом,
+        // но корутина дождется выполения getRandomNumberAfterDelay в состоянии Cancelling (зависнем на 3 секунды на join).
+        // {1} выполнится, {2} не выполнится
+        globalJob.cancel()
+        log("globalJob cancelled")
+
+        // Если же отменить скоуп repository, то ожидающий его globalJob тоже сразу же отменится.
+        // {1} и {2} не выполнятся
+        //repository.job.cancel()
+        //log("Repository job cancelled")
+
+        globalJob.dumpWithChild("globalJob> ")
+        globalJob.join()
+        log("globalJob.join() complete")
+    }
+}
